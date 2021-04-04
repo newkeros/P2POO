35a5dad4a8789af7ea9f37ca73c218c9d45f272d
newkeros <60075901+newkeros@users.noreply.github.com>
2021-04-04 13:21:20 +0200

    Delete categories.py

--- categories.py
+++
@@ -1,7 +0,0 @@
-class Category:
-    def __init__(self):
-        récuperer tous les liens
-        mettre dans une instance de liste
-        "methode run boucle sur tous les livres, créer book handle et print book"
-handle qui recupere tous les liens des categories
-run qui boucle sur cette liste et qui fera le print book des catégories
